c3f3ea96058a9320366f4afde24d2a249c1cf872
Alberto <67006178+ebir0aroca@users.noreply.github.com>
2022-08-16 11:38:15 +0200

    Update engine.py

--- core/engine.py
+++ core/engine.py
@@ -1,6 +1,12 @@
 import datetime
 import csv
 
+SCRAP_META = [['scrap_meta', 'guid'], ['scrap_meta', 'date_start'],
+            ['scrap_meta', 'maincategory_url'], ['scrap_meta', 'spider_country'],
+            ['scrap_meta', 'spider_date_end'], ['scrap_meta', 'spider_marketplace'],
+            ['scrap_meta', 'spider_name'], ['scrap_meta', 'spider_version'],
+            ['scrap_meta', 'title'], ['scrap_meta', 'spider_date_start']]
+
 def log_error(LOG_FILEPATH, error):
   el = [datetime.datetime.now().strftime('%D %T'), error]
   with open(LOG_FILEPATH, 'a', newline='') as f:
