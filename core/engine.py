67fe7b399aec19a6eff8951efd3dcf49f0c6aace
Alberto <67006178+ebir0aroca@users.noreply.github.com>
2022-08-16 18:50:45 +0200

    Update engine.py

--- core/engine.py
+++ core/engine.py
@@ -30,6 +30,7 @@ class Application:
   MAIN_DB_FILEPATH = ""
   log_problems = True
   overwrite_main_database = True
+  min_show_error_level = 1 #equals and greater
   
   
   SCRAP_META = [['scrap_meta', 'guid'], ['scrap_meta', 'date_start'],
@@ -120,15 +121,153 @@ class Application:
         print(f'Database file created: {self.MAIN_DB_FILEPATH}')
         main_database.to_csv(self.MAIN_DB_FILEPATH)
 
-  def add_log_error(error):
-    el = [datetime.datetime.now().strftime('%D %T'), error]
+  def add_log_error(error, level):
+    # 0: informative
+    # 1: mistakes
+    # 2: error
+    # 3: critical
+    el = [level, datetime.datetime.now().strftime('%D %T'), error]
     with open(self.LOG_FILEPATH, 'a', newline='') as f:
         cw = csv.writer(f)
         cw.writerow(el)
+    if(level>=self.min_show_error_level) 
+        print(error)
         
   def get_scrap_filelist(self):
     scrap_list = []
     for scrap_file_name in next(os.walk(self.SCRAP_PATH), (None, None, []))[2]:
         scrap_list.append(os.path.join(self.SCRAP_PATH, scrap_file_name)) 
     return scrap_list
+
+  def translate_categories(self, scrap_df, categories_translate_tf): 
+    # 1.1 Splits Breadcrumbs into categories for a given scrap dataframe  
+    for i, row in scrap_df.iterrows():
+      if(type(scrap_df.at[i,'breadcrumbs'])==list):
+          for j in range(len(scrap_df.at[i,'breadcrumbs'])):
+            scrap_df.at[i,'category'+str(j)] = scrap_df.at[i,'breadcrumbs'][j]
+    print("Breadcrumbs split to categories. ")
+
+
+    # 1.2. Translate categories from different Marketplace, Country and Language
+    # marketplace                     (e.g. hornbach)
+    #   country                       (e.g. ch)
+    #     target_col_name             (e.g. category)
+    #       target_col_value          (e.g. Illuminated bathroom mirror)
+    # 			  rule_name               (e.g. R48)
+    #  results ---> index             (e.g. 111)
+    marketplace, country= scrap_df.tail(1)["scrap_meta.spider_marketplace"].values[0], scrap_df.tail(1)["scrap_meta.spider_country"].values[0]
+    print("Marketplace and country from the scrap data: " + marketplace + "." + country)
+    result_df = pd.DataFrame()
+    
+    rules0 = (categories_translate_tf['marketplace']==marketplace) & (categories_translate_tf['country']==country)
+    target_col_names = categories_translate_tf[rules0]['target_col_name'].unique()
+
+    for target_col_name in target_col_names:
+      rules1 = (rules0) & (categories_translate_tf['target_col_name']==target_col_name)
+      target_col_values = categories_translate_tf[rules1]['target_col_value'].unique()
+
+      for target_col_value in target_col_values:
+        rules2 = (rules1) & (categories_translate_tf['target_col_value']==target_col_value)
+        rule_names = categories_translate_tf[rules2]['rule_name'].unique()
+        
+        for rule_name in rule_names:
+          rules3 = (rules2) & (categories_translate_tf['rule_name']==rule_name)
+          filters = categories_translate_tf[rules3]['filter_name'].index.values
+          
+          #each new rule
+          isFirst = True
+          joined_rules = True
+          avoidRule = False
+          
+          for filter in filters:
+            filter_name   = categories_translate_tf.loc[filter]['filter_name']
+            filter_value  = categories_translate_tf.loc[filter]['filter_value']
+            #print("           {} {} {} {} {} {} {}".format(marketplace,	country,	target_col_name,	target_col_value,	rule_name, filter_name,	filter_value))
+            
+            joined_df = pd.DataFrame()
+
+            if isFirst:
+              joined_rules = True 
+              isFirst = False
+            
+            #Checks ...
+            if filter_name in scrap_df:
+              avoidRule = False
+              joined_rules = (joined_rules) & (scrap_df[filter_name] == filter_value)
+
+            else:
+              avoidRule = True 
+              self.add_log_error(":::RULE IS WRONG. THE FILTER_NAME DOES NOT CORRESPOND WITH ANY SCRAPPED COLUMN :::", 1)
+              self.add_log_error("    Please check the configuration file", 1)
+              self.add_log_error("    Rule Name: {}".format(rule_name), 1)            
+              self.add_log_error("    Scrapper {}.{} ".format(marketplace, country), 1)
+              self.add_log_error("        Target: ['{}'] == '{}']  ".format(target_col_name, target_col_value), 1)
+              self.add_log_error("        Filter: ['{}'] == '{}'] ".format(filter_name, filter_value), 1)
+          
+          if not avoidRule:
+            #create a copy to prevent pandas' SettingWithCopyWarning
+            joined_df = scrap_df[joined_rules].copy() 
+            
+            if len(joined_df) !=  0:
+              joined_df.loc[:, target_col_name] =  target_col_value
+              
+            else:
+              self.add_log_error(":::THE RESULT IS EMPTY, MAKE SURE THERE'S NO DATA IN THE WEBSITE:::", 1)
+              self.add_log_error("    Rule Name: {}".format(rule_name), 1)
+              self.add_log_error("    Scrapper {}.{} ".format(marketplace, country), 1)
+              self.add_log_error("        Target: ['{}'] == '{}']  ".format(target_col_name, target_col_value), 1)
+              self.add_log_error("        Filter: ['{}'] == '{}']  ".format(filter_name, filter_value), 1)
+            result_df = result_df.append(joined_df)
+
+    #engine.add_log_error
+    return result_df
+
+  def set_default_values(self, scrap_df):   
+    scrap_df.loc[:,"creation_date"] = pd.to_datetime(scrap_df['scrap_meta.spider_date_start']).dt.date
+    scrap_df['img_url'] = ''
+    scrap_df.loc[:,'img_url'] = ''
+    scrap_df.loc[:,'seller'] = 'Store'
+    scrap_df.loc[:,'isStoreBrand'] = False
+    scrap_df.loc[:,'currency'] = scrap_df['currency'].replace('€', 'EUR')
+
+    #NaN values
+    scrap_df.loc[:,'brand'] = scrap_df['brand'].fillna('')
+    scrap_df.loc[:,'specialPrice'] = scrap_df['specialPrice'].replace(np.nan,  "0.00")
+    scrap_df.loc[:,'specialPrice'] = scrap_df['specialPrice'].apply(datamining.remove_chars)
+    scrap_df.loc[:,'specialPrice'] = scrap_df['specialPrice'].replace("", "0.00")   
+    scrap_df.loc[:,'price'] = scrap_df['price'].replace(np.nan, 0.00) 
+    scrap_df.loc[:,'price'] = scrap_df['price'].apply(datamining.remove_chars)  
+    scrap_df.loc[:,'price'] = scrap_df['price'].replace("",  0.00)
+    scrap_df.loc[:,'reviews_rating'] = scrap_df['reviews_rating'].replace("",  0.00) 
+    scrap_df.loc[:,'reviews_rating'] = scrap_df['reviews_rating'].replace(np.nan, 0.00)
+    scrap_df.loc[:,'reviews_count'] = scrap_df['reviews_count'].replace("", 0)
+    scrap_df.loc[:,'reviews_count'] = scrap_df['reviews_count'].replace(np.nan, 0.00)
+
+    return scrap_df
+
+
+  def set_isStoreBrand(self, scrap_df, store_brands):
+    scrap_df.loc[:,'isStoreBrand']=scrap_df['brand'].isin(store_brands)
+    return scrap_df
+
+
+  def delete_irrelevant_data(self, scrap_df):
+    # Some data clean up
+    del_columns = [col for col in scrap_df if 'Unnamed' in col]
+    scrap_df.drop(columns=del_columns, inplace=True)
+
+    #delete not used cols
+    del_columns = [col for col in scrap_df if 'specs.' in col]
+    scrap_df.drop(columns=del_columns, inplace=True)
+    del_columns = [col for col in scrap_df if 'custom.' in col]
+    scrap_df.drop(columns=del_columns, inplace=True)
+
+    # drop categories
+    scrap_df.drop(columns=['category0', 'category1', 'category2', 'category3', 'category4', 'category5', 'category6'],
+                inplace=True, errors='ignore')
+    
+    return scrap_df
+
+  def set_data_types(self, scrap_df, product_datamodel):
+    return scrap_df.astype(product_datamodel)
 
